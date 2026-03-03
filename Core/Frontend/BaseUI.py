ac71eac3b04eb08b4d55f21f60ff708a63f59ecc
WASDDestroy <3288211880@qq.com>
2026-03-03 13:13:11 +0800

    Remove wrong code for listening keyboard input; Enhanced dynamic import function to allow static import in submodules; add a method confirmOperation to simplify the process of asking user confirmation

--- Core/Frontend/BaseUI.py
+++ Core/Frontend/BaseUI.py
@@ -45,46 +45,6 @@ class BaseUI:
         '''
         pass
 
-    def listenKeyInput(self):
-        if os.name == "nt":
-            try:
-                import msvcrt
-            except:
-                self.myLogger.log("W", "Failed to import msvcrt, some functions may fail to work.")
-        else:
-            try:
-                import tty
-                import termios
-            except:
-                self.myLogger.log("W", "Failed to import tty or termios, some functions may fail to work.")
-        if os.name == "nt":
-            ch = msvcrt.getch()
-            if ch == b'\xe0':
-                second = msvcrt.getch()
-                key_map = {
-                    b'H': 'UP',
-                    b'P': 'DOWN',
-                    b'K': 'LEFT',
-                    b'M': 'RIGHT'
-                }
-                return key_map.get(second, None)
-            elif ch == b'\r':
-                return 'ENTER'
-            elif ch == b' ':
-                return 'SPACE'
-            return ch.decode('utf-8', errors='ignore')
-        else:
-            fd = sys.stdin.fileno()
-            old_settings = termios.tcgetattr(fd)
-            try:
-                tty.setraw(fd)
-                ch = sys.stdin.read(1)
-                if ch == '\x1b':
-                    ch += sys.stdin.read(2)
-                return ch
-            finally:
-                termios.tcgetattr(old_settings)
-
     def _pressEnterToContinue(self):
         input("Press Enter to continue.")
 
@@ -96,21 +56,43 @@ class BaseUI:
         self.myLogger.log("D", "Importing module: " + moduleName, self.TAG)
         self.myLogger.log("D", "Complete path: " + os.path.join(moduleDir, moduleName + ".py"), self.TAG)
         try:
-            spec : importlib.util.__spec__ = importlib.util.spec_from_file_location(moduleName,
+            if moduleName in sys.modules:
+                self.myLogger.log("D", f"Module {moduleName} already imported, returning existing module", self.TAG)
+                return sys.modules[moduleName]
+            if moduleDir not in sys.path:
+                sys.path.insert(0, moduleDir)
+                self.myLogger.log("D", f"Added {moduleDir} to sys.path", self.TAG)
+            try:
+                ImportedModule = importlib.import_module(moduleName)
+                self.myLogger.log("I", f"Successfully imported module {moduleName} using import_module", self.TAG)
+            except ImportError:  
+                spec : importlib.util.__spec__ = importlib.util.spec_from_file_location(moduleName,
                                                                                     location = os.path.join(moduleDir, moduleName + ".py"))
-            ImportedModule = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(ImportedModule)
+                ImportedModule = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(ImportedModule)
             sys.modules[moduleName] = ImportedModule
+            self.myLogger.log("I", "Successfully imported module %s from %s"%(moduleName, moduleDir), self.TAG)
             return ImportedModule
         except Exception as e:
             self.myLogger.log("W", "Exception happened when importing module: " + repr(e), self.TAG)
+            return None
     
     def _createInstance(self, module, className, logger):
-        ClassName = getattr(module, className)
+        try:
+            ClassName = getattr(module, className)
+        except AttributeError:
+            self.myLogger.log("E", "Failed when creating instance for module with class name %s, no such attribution!"%(className), self.TAG)
+            raise RuntimeError("Unable to create instance.")
+        self.myLogger.log("I", "Successfully created instance for module with classname " + className, self.TAG)
         return ClassName(logger = logger)
     
     def _createFrontendInstance(self, module, className, logger, gotoNode):
-        ClassName = getattr(module, className)
+        try:
+            ClassName = getattr(module, className)
+        except AttributeError:
+            self.myLogger.log("E", "Failed when creating instance for module with class name %s, no such attribution!"%(className), self.TAG)
+            raise RuntimeError("Unable to create instance.")
+        self.myLogger.log("I", "Successfully created instance for module with classname " + className, self.TAG)
         return ClassName(logger = logger, gotoNode = gotoNode)
 
     def getNodeFunctions(self):
@@ -137,6 +119,15 @@ class BaseUI:
     def callBackEnd(self, functionName : str):
         self.handleBackAndExit(functionName)
         raise NotImplementedError("Unimplemented method callBackEnd. Module: " + self.TAG)
+
+    def confirmOperation(self, prompt = "Confirm operation?") -> bool:
+        try:
+            if input(prompt + " [y/N]: ").lower() == "y":
+                return True
+            else:
+                return False
+        except:
+            return False
     
     def showTitle(self):
         tmpLen = (80 - len(self.myNavigationEngine.currentNodeName)) // 2
