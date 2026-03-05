2f670a63c85073b568da557f0057a64bfdcaf917
WASDDestroy <3288211880@qq.com>
2026-03-05 12:37:49 +0800

    Migrate Import and export logic to file ImportConfigUI.py and ExportConfigUI.py

--- Core/Frontend/ConfigManagerUI.py
+++ Core/Frontend/ConfigManagerUI.py
@@ -10,52 +10,22 @@ class ConfigManagerUI(BaseUI.BaseUI):
                                    "P" : "Save current config as a persistent one"}
 
     def callBackEnd(self, functionName: str):
-        functionNameTuple = ("Import Config",
-                             "Export Config",
-                             "Config Library Manager",
+        functionNameTuple = ("Config Library Manager",
                              "Set a config active",
                              "Save current config as a persistent one")
         self.myConfigManager = self._createInstance(self.configManagerModule,
                                                     "ConfigManager",
                                                     self.myLogger)
-        if functionName == functionNameTuple[0]:
-            importFileName = self.__importConfigFromFileUI()
-            if importFileName is None:
-                print("Cancelled.")
-                return
-            archiveType = self.myConfigManager.checkConfigType(fileName = importFileName)
-            if archiveType == "SINGLE":
-                try:
-                    self.myConfigManager.importSingleConfig(importFromFileName = importFileName)
-                    print("Successfully imported config.")
-                except Exception as e:
-                    self.myLogger.log("W", e, self.TAG)
-                    print("Import failed!")
-                self._pressEnterToContinue()
-            elif archiveType == "BATCH":
-                try:
-                    self.myConfigManager.batchImportConfig(importFromFileName = importFileName)
-                    print("Successfully imported config.")
-                except Exception as e:
-                    self.myLogger.log("W", e, self.TAG)
-                    print("Import failed!")
-                self._pressEnterToContinue()
-            else:
-                print("Invalid archive file.")
-                self._pressEnterToContinue()
+        if functionName == functionNameTuple[0]: # Manage
+            print("Function is in development.")
+            self._pressEnterToContinue()
         elif functionName == functionNameTuple[1]:
-            print("Function is in development.")
-            self._pressEnterToContinue()
-        elif functionName == functionNameTuple[2]:
-            print("Function is in development.")
-            self._pressEnterToContinue()
-        elif functionName == functionNameTuple[3]:
             configToActive = self.__setConfigActiveUI()
             if configToActive:
                 self.myConfigManager.setConfigActive(configToActive)
             else:
                 print("User cancelled operation.")
-        elif functionName == functionNameTuple[4]:
+        elif functionName == functionNameTuple[2]:
             configName = input("Enter the name of your new config.")
             result = self.myConfigManager.saveAsPersistentConfig(configName)
             if result:
@@ -89,39 +59,5 @@ class ConfigManagerUI(BaseUI.BaseUI):
         print("Set this config active? [y/N]", end = " ")
         if input().upper() == "Y":
             return configNames[inputNumber - 1]
-        else:
-            return None
-
-    def __importConfigFromFileUI(self):
-        """
-        Provide filename of zip archive to be imported. Will return None if user cancels.
-        """
-        print("Place zip file under root directory and press Enter to continue.")
-        self._pressEnterToContinue()
-        rootDir = os.getcwd()
-        fileCanBeImported = []
-        for i in os.listdir(rootDir):
-            if i.endswith(".zip"):
-                fileCanBeImported.append(i)
-        for i in range(len(fileCanBeImported)):
-            print(i + 1, fileCanBeImported[i])
-        print("Select a file to import with number. Enter -1 to cancel.")
-        while 1:
-            myInput = input("Your choice: ")
-            try:
-                inputNumber = int(myInput)
-                if 0 < inputNumber <= len(fileCanBeImported):
-                    print("Import file: %s"%(fileCanBeImported[inputNumber - 1]))
-                    break
-                elif inputNumber == -1:
-                    return None
-                else:
-                    raise IndexError
-            except Exception as e:
-                print("Invalid input, try again.")
-                self.myLogger.log("W", "Invalid input when determining config to import: " + repr(e), self.TAG)
-        print("Import this file? [y/N]", end = " ")
-        if input().upper() == "Y":
-            return fileCanBeImported[inputNumber - 1]
         else:
             return None
