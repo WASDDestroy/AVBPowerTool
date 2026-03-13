e7f388527b850d58c95d2bf13a51c00866d61e71
WASDDestroy <3288211880@qq.com>
2026-03-13 23:55:14 +0800

    Add more information displayed when switching configs; Use new file selector

--- Core/Frontend/ConfigManagerUI.py
+++ Core/Frontend/ConfigManagerUI.py
@@ -1,11 +1,9 @@
 import BaseUI
+from Core.Frontend.UIUtils import EnhancedFileSelectorUI
 
 
+# noinspection PyAttributeOutsideInit
 class ConfigManagerUI(BaseUI.BaseUI):
-
-    def __init__(self, logger=None, goto_node="", navigation_engine=None):
-        super().__init__(logger, goto_node, navigation_engine)
-        self.myConfigManager = None
 
     def customized_init(self):
         self.TAG = "ConfigManagerUI"
@@ -13,29 +11,37 @@ class ConfigManagerUI(BaseUI.BaseUI):
         self.configManagerModule = self.my_importer.import_module(
             "ConfigManager.py")
         # noinspection PyAttributeOutsideInit
-        self.customizedFunction = {"S": "Set a config active",
+        self.customized_function = {"S": "Set a config active",
                                    "P": "Save current config as a persistent one"}
 
     def call_backend(self, function_name: str):
-        function_name_tuple = ("Config Library Manager",
-                             "Set a config active",
+        function_name_tuple = ("Set a config active",
                              "Save current config as a persistent one")
         self.myConfigManager = self.my_importer.create_instance(self.configManagerModule,
                                                               "ConfigManager",
                                                                 self.my_logger)
-        if function_name == function_name_tuple[0]:  # Manage
-            self._in_development_placeholder()
-        elif function_name == function_name_tuple[1]:
-            config_to_active = self. my_ui_utils.select_config_ui()
+        if function_name == function_name_tuple[0]:
+            config_names = self.myConfigManager.get_all_configs()
+            my_selector = EnhancedFileSelectorUI("Select a Config to Activate", config_names, False, self.my_logger)
+            config_to_active = my_selector.show()[0]
             if config_to_active:
-                self.myConfigManager.set_config_active(config_to_active)
+                if self.myConfigManager.set_config_active(config_to_active):
+                    print("Successfully switched active config to", config_to_active)
+                    print("Old \"active\" config has been removed.")
+                else:
+                    print("Failed to set active config to", config_to_active)
+                    print("Please refer to log file for further information.")
+                    print("Note: Exit tool first, then check log file, otherwise nothing will be shown in latest log.")
             else:
                 print("User cancelled operation.")
-        elif function_name == function_name_tuple[2]:
+            self.my_ui_utils.press_enter_to_continue()
+        elif function_name == function_name_tuple[1]:
             config_name = input("Enter the name of your new config: ")
             result = self.myConfigManager.save_as_persistent_config(config_name)
             if result:
-                print("Success.")
+                print("Successfully saved \"current\" config to persistent file, name: %s." % config_name)
             else:
-                print("Failed.")
+                print("Failed to save \"current\" config to persistent file.")
+                print("Please refer to log file for further information.")
+                print("Note: Exit tool, then check log file, otherwise nothing will be shown in latest log.")
             self.my_ui_utils.press_enter_to_continue()
