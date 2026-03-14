9a84e04604376366727a59ebcb03e8a1b68fd56b
WASDDestroy <3288211880@qq.com>
2026-03-14 16:19:05 +0800

    Adjust import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import time
 import Core.Frontend.HomePageUI as HomePageUI
 import Core.EnvironmentChecker as EnvironmentChecker
-from Core import LogUtils
+import Core.LogUtils as LogUtils
 
 def print_logo():
     try:
