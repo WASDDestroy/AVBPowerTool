92bca4d3c5a021dd64383337bf77d4e274d22844
WASDDestroy <3288211880@qq.com>
2026-03-14 14:17:25 +0800

    Use static import instead of DynamicImportUtils; Shorten countdown before starting sign process

--- Core/Frontend/SignAllImagesUI.py
+++ Core/Frontend/SignAllImagesUI.py
@@ -1,29 +1,20 @@
-import BaseUI
 import os
 import time
-import json
 
+import BaseUI
+import Core.SignImages as SignImages
 
 class SignAllImagesUI(BaseUI.BaseUI):
 
     def customized_init(self):
-        self.TAG = "SignAllImagesUI"
         self.customized_function = {
             "Y": "Sign all images with current config file",
             "I": "Sign selected image file"
         }
-        # print("Using navigation map: ", self.myNavigationEngine.currentFileDir)
-        # print(self.myNavigationEngine.currentDic)
-        # print(self.customizedFunction)
-        # print(self.nodeFunction)
-        # print(self.myNavigationEngine.getNextNodeNames())
-        # noinspection PyAttributeOutsideInit
-        self.SignImages = self.my_importer.import_module("SignImages.py")
 
     def call_backend(self, function_name: str):
         if function_name == "Sign all images with current config file":
-            my_object = self.my_importer.create_instance(
-                self.SignImages, "SignImages", self.my_logger)
+            my_signer = SignImages.SignImages(self.my_logger)
             if os.name == "nt":
                 print(
                     "WARNING: YOU CANNOT ADD HASHTREE FOOTER WITH FEC ROOTS WHEN RUNNING ON WINDOWS")
@@ -37,40 +28,14 @@ class SignAllImagesUI(BaseUI.BaseUI):
             print()
             print("It may take up to minutes depending on your hardware config.")
             print("The program is still running normally, DO NOT KILL IT!")
-            for i in range(5):
-                print("Start signing after %d secs." % (5 - i))
+            for i in range(3):
+                print("Start signing after %d secs." % (3 - i))
                 time.sleep(1)
             print()
-            my_object.sign_images_batch()
+            my_signer.sign_images_batch()
             self.my_ui_utils.press_enter_to_continue()
         elif function_name == "Sign selected image file":
             self._in_development_placeholder()
-
-    def __choose_images_to_sign(self):
-        image_to_display: set = self.__get_available_image_files()
-        self.my_ui_utils.clear_screen()
-        print("=" * 80)
-        for i in image_to_display:
-            print("%40s [ ]" % i)
-        print("0 Selected.")
-
-    @staticmethod
-    def __get_available_image_files():
-        with open(
-            os.path.join(os.getcwd(), "Core",
-                         "currentConfigs", "imageInfo.json"),
-            "r",
-                encoding="UTF-8") as myFile:
-            image_config_dict: dict = json.load(myFile)
-            image_available_in_config: list = list(image_config_dict.keys())
-        image_dir = os.path.join(os.getcwd(), "Images")
-        image_available_in_work_dir = []
-        for i in os.listdir(image_dir):
-            if "vbmeta" not in i:
-                image_available_in_work_dir.append(i)
-        set1 = set(image_available_in_config)
-        set2 = set(image_available_in_work_dir)
-        return set1 & set2
 
     @staticmethod
     def __is_wsl():
