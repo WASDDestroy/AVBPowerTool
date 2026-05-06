3b15dfc20977cb3a9e085d55882bf03033ded894
WASDDestroy <3288211880@qq.com>
2026-05-06 13:14:11 +0800

    Remove "Read info of all images" choice in ReadImageInfoUI.py, Frontend will only provide a choice with selector that allows "select all" to achieve similar goal

--- Core/Frontend/ReadImageInfoUI.py
+++ Core/Frontend/ReadImageInfoUI.py
@@ -11,8 +11,7 @@ class ReadImageInfoUI(BaseUI.BaseUI):
     def customized_init(self):
         self.TAG = "ReadImageInfoUI"
         self.customized_function = {
-            "A": "Read info of all images",
-            "S": "Read info of selected image(s) (NOT RECOMMENDED)",
+            "S": "Read info of selected image(s)",
         }
         # noinspection PyAttributeOutsideInit
         self.my_image_info_utils = ImageInfoUtils.ImageInfoUtils()
@@ -20,9 +19,9 @@ class ReadImageInfoUI(BaseUI.BaseUI):
         self.m_config_parser = ConfigParser.ConfigParser()
 
     def call_backend(self, function_name: str):
-        if function_name == "Read info of all images":
-            self.__handle_read_all_images_info()
-        elif function_name == "Read info of selected image(s) (NOT RECOMMENDED)":
+        # if function_name == "Read info of all images":
+        #     self.__handle_read_all_images_info()
+        if function_name == "Read info of selected image(s)":
             self.__handle_read_selected_images_info()
         self.my_ui_utils.press_enter_to_continue()
 
@@ -36,7 +35,8 @@ class ReadImageInfoUI(BaseUI.BaseUI):
                 print("Reading selected images.")
                 self.my_logger.log("I", "Read selected image(s).", self.TAG)
                 for i in range(len(images_to_read)):
-                    images_to_read[i] = images_to_read[i].rstrip(".img")
+                    if images_to_read[i].endswith(".img"):
+                        images_to_read[i] = images_to_read[i][:-4]
                 self.my_image_info_utils.read_image_info_batch(images_to_read)
                 print("Successfully read info of selected images.")
             else:
@@ -46,37 +46,37 @@ class ReadImageInfoUI(BaseUI.BaseUI):
         else:
             self.my_ui_utils.message_on_cancel()
 
-    def __handle_read_all_images_info(self):
-        if self.confirm_operation():
-            check_result = self.my_image_info_utils.check_image_exists(
-                image_info_list=self.m_config_parser.get_image_list())
-            if not check_result[0]:
-                print("WARNING: Image mismatch!")
-                if check_result[1] == "MORE":
-                    print("These images are unnecessary, consider remove them:")
-                    for i in check_result[2]:
-                        print(i)
-                elif check_result[1] == "LESS":
-                    print(
-                        "These images are missing, you must have them to continue process:")
-                    for i in check_result[2]:
-                        print(i)
-                elif check_result[1] == "DIFF":
-                    print("Necessary image(s) not found!")
-                    print("Config list:")
-                    for i in self.m_config_parser.get_image_list():
-                        print(i)
-                    print("You have these images:")
-                    for i in check_result[3]:
-                        print(i)
-                return
-            else:
-                try:
-                    print("Reading AVB information of all images.")
-                    self.my_image_info_utils.read_image_info_batch(
-                        self.m_config_parser.get_image_list())
-                    print("Successfully read info of all images.")
-                except:
-                    print("Operation failed.")
-        else:
-            self.my_ui_utils.message_on_cancel()
+    # def __handle_read_all_images_info(self):
+    #     if self.confirm_operation():
+    #         check_result = self.my_image_info_utils.check_image_exists(
+    #             image_info_list=self.m_config_parser.get_image_list())
+    #         if not check_result[0]:
+    #             print("WARNING: Image mismatch!")
+    #             if check_result[1] == "MORE":
+    #                 print("These images are unnecessary, consider remove them:")
+    #                 for i in check_result[2]:
+    #                     print(i)
+    #             elif check_result[1] == "LESS":
+    #                 print(
+    #                     "These images are missing, you must have them to continue process:")
+    #                 for i in check_result[2]:
+    #                     print(i)
+    #             elif check_result[1] == "DIFF":
+    #                 print("Necessary image(s) not found!")
+    #                 print("Config list:")
+    #                 for i in self.m_config_parser.get_image_list():
+    #                     print(i)
+    #                 print("You have these images:")
+    #                 for i in check_result[3]:
+    #                     print(i)
+    #             return
+    #         else:
+    #             try:
+    #                 print("Reading AVB information of all images.")
+    #                 self.my_image_info_utils.read_image_info_batch(
+    #                     self.m_config_parser.get_image_list())
+    #                 print("Successfully read info of all images.")
+    #             except:
+    #                 print("Operation failed.")
+    #     else:
+    #         self.my_ui_utils.message_on_cancel()
