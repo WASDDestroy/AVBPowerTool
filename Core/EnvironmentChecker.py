4eedb805907f8277d0205a486d048f2ee52d9954
WASDDestroy <3288211880@qq.com>
2026-05-12 20:43:51 +0800

    Migrate environment setup code to EnvironmentChecker.py

--- Core/EnvironmentChecker.py
+++ Core/EnvironmentChecker.py
@@ -1,6 +1,15 @@
 import os
+import platform
+import shutil
 import subprocess
+import sys
 import threading
+
+from Core.GlobalConfigUtils import GlobalConfigInfo
+from Core.LogUtils import ConsoleLog as cLog
+from Core.LogUtils import LogUtils
+
+global_config = GlobalConfigInfo()
 
 class EnvironmentChecker:
     @staticmethod
@@ -97,12 +106,28 @@ class EnvironmentChecker:
 
     @staticmethod
     def check_fec_state():
+        return False
+        # Check system-level fec first
         # if os.name == "nt":
         #     return False
         # else:
-        #     subprocess.run(["command", "-v", "fec"])
-        pass
-    
+        #     # noinspection PyDeprecation
+        #     path = shutil.which("fec")
+        #     if path:
+        #         return True
+        # return False
+
+    @staticmethod
+    def is_packed_fec_available():
+        FEC_PATH = os.path.join(global_config.get_value("bin_dir"), "fec")
+        # Check packed fec
+        architecture_name = platform.architecture()
+        if architecture_name == "AMD64" and os.path.exists(os.path.join(FEC_PATH, "x86_64", "fec")):
+            return True
+        if architecture_name == "aarch64" and os.path.exists(os.path.join(FEC_PATH, "aarch64", "fec")):
+            return True
+        return False
+
     @staticmethod
     def check_libs():
         """
@@ -158,3 +183,69 @@ class EnvironmentInfo:
         if self.python_command is None:
             self.python_command = EnvironmentChecker.detect_python_command()
         return self.python_command
+
+class EnvironmentSetup:
+
+    __TAG = "EnvironmentSetup"
+
+    @staticmethod
+    def check_libraries(should_install=True, should_check=True):
+        if not should_check:
+            return
+        missing_libs = EnvironmentChecker.check_libs()[1]
+        if missing_libs:
+            missing_libs_string = ""
+            for i in missing_libs:
+                missing_libs_string += i + " "
+            print("Missing lib(s):", missing_libs_string)
+            if should_install:
+                print("Installing dependencies automatically.")
+                try:
+                    import subprocess
+                    subprocess.run(["pip", "install"] + missing_libs)
+                except ImportError:
+                    print("Failed to import subprocess, exiting.")
+                    exit(1)
+                except Exception as e:
+                    print("Unhandled exception:", e)
+                    exit(1)
+            else:
+                print("Run pip install " + missing_libs_string)
+                exit(1)
+
+    def check_necessary_folders(self):
+        logger = LogUtils()
+        try:
+            EnvironmentChecker.check_necessary_folders(logger)
+            # print("Folder check passed.")
+            logger.log("I", "Folder check passed.", self.__TAG)
+        except Exception as e:
+            cLog.fatal("Exception happened when checking necessary folders: " + str(e))
+            logger.log("F", "Exception happened when checking necessary folders: " + str(e), self.__TAG)
+            exit(1)
+
+    def add_frontend_dir_to_path(self):
+        logger = LogUtils()
+        try:
+            if os.path.join(os.getcwd(), "Core", "Frontend") not in sys.path:
+                # print("Adding frontend dir to system path.")
+                logger.log("I", "Adding frontend dir to system path.", self.__TAG)
+                sys.path.insert(0, os.path.join(os.getcwd(), "Core", "Frontend"))
+        except Exception as e:
+            cLog.fatal("Exception happened when processing frontend folder: " + str(e))
+            logger.log("F", "Exception happened when processing frontend folder: " + str(e), self.__TAG)
+            exit(1)
+
+    @staticmethod
+    def setup_fec():
+        # fec_dir = "/usr/local/bin"
+        # deps_dir = "/usr/lib"
+        logger = LogUtils()
+        fec_state = EnvironmentChecker.check_fec_state()
+        logger.info("FEC state: " + str(fec_state))
+        packed_fec_availability = EnvironmentChecker.is_packed_fec_available()
+        logger.info("Packed FEC availability: " + str(packed_fec_availability))
+        if (not fec_state) and packed_fec_availability:
+            logger.info("Set up fec environment with packed fec")
+
+
